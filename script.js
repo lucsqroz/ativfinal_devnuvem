06a3d9e8139c5649770407d0b334539b7c3823f1
lucsqroz <116466858+lucsqroz@users.noreply.github.com>
2024-03-18 16:49:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById('pontoColetaForm').addEventListener('submit', async (e) 
   const cep = document.getElementById('cep').value;
   const bairro = document.getElementById('bairro').value;
 
-  const response = await fetch('https://83b19784-dda7-4d9e-975e-7a93f3b82e1c-00-1ehewumuks9f3.spock.replit.dev//pontos-coleta', {
+  const response = await fetch('https://83b19784-dda7-4d9e-975e-7a93f3b82e1c-00-1ehewumuks9f3.spock.replit.dev/pontos-coleta', {
       method: 'POST',
       headers: {
           'Content-Type': 'application/json'
@@ -19,7 +19,7 @@ document.getElementById('pontoColetaForm').addEventListener('submit', async (e) 
 });
 
 async function carregarPontosColeta() {
-  const response = await fetch('https://83b19784-dda7-4d9e-975e-7a93f3b82e1c-00-1ehewumuks9f3.spock.replit.dev/');
+  const response = await fetch('https://83b19784-dda7-4d9e-975e-7a93f3b82e1c-00-1ehewumuks9f3.spock.replit.dev/pontos-coleta');
   const pontosColeta = await response.json();
 
   pontosColeta.forEach(adicionarPontoNaLista);
@@ -33,3 +33,4 @@ function adicionarPontoNaLista(ponto) {
 }
 
 carregarPontosColeta();
+
